c338cbf51c484caed65b73c855b920af2886931c
Yoyoknuckles <39626900+Yoyoknuckles@users.noreply.github.com>
2018-05-26 00:37:56 +0300

    Create package.py

---
+++ package.py
@@ -0,0 +1,13 @@
+{
+  "name": "Test",
+  "description": "Test",
+  "version": "0.0.0",
+  "main": "botchan.py",
+  "scripts": {
+    "start": "node botchan.py"
+  },
+  "dependencies": {
+    "discord.py": "11.1.0",
+    "request": "2.81.0"
+  }
+}
